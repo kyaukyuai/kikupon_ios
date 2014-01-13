fb6296d3bc51ca6f0598d0618f8a150c4f72dd3c
nab <napu1031@gmail.com>
2014-01-13 18:59:36 +0900

    fix login function

--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -2,57 +2,35 @@ class TapController < UIViewController
   def viewDidLoad
     super
 
+
     self.view.backgroundColor = UIColor.whiteColor
 
-    @label = UILabel.alloc.initWithFrame(CGRectZero)
-    @label.text = "Tap(#{self.navigationController.viewControllers.count})"
-    @label.sizeToFit
-    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
-    self.view.addSubview @label
+     @label = UILabel.alloc.initWithFrame(CGRectZero)
+     @label.text = "きくぽん"
+     @label.sizeToFit
+     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+     self.view.addSubview @label
 
-    self.title = "Tap(#{self.navigationController.viewControllers.count})"
+    self.title = "ログイン"
     right_button = UIBarButtonItem.alloc.initWithTitle("Push", style: UIBarButtonItemStyleBordered, target:self, action:'push')
     self.navigationItem.rightBarButtonItem = right_button
 
+    hoge_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    hoge_button.backgroundColor = UIColor.grayColor
+    hoge_button.sizeToFit
+    hoge_button.frame = CGRectMake(20, 400, self.view.frame.size.width-40, 50)
+    hoge_button.setTitle("Connect with Twitter", forState: UIControlStateNormal)
+    hoge_button.tintColor = UIColor.blackColor
+    hoge_button.addTarget(self, action:'connectTwitter', forControlEvents:UIControlEventTouchUpInside)
+    self.view.addSubview hoge_button
 
-
-
-
-=begin
-    @account_store = ACAccountStore.alloc.init
-    @account_type = @account_store.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierFacebook);
-    @options = {ACFacebookAppIdKey       => '654511364587200',
-                ACFacebookPermissionsKey => ['email'],
-               }
-    @account_store.requestAccessToAccountsWithType(@account_type, options: @options, completion: lambda do |granted, error|
-                                                                                                   if granted
-                                                                                                       accounts = @account_store.accountsWithAccountType(@account_type)
-                                                                                                       anAccount = accounts.lastObject
-                                                                                                       NSLog(anAccount("#{anAccount.description}"))
-                                                                                                   else
-                                                                                                       NSLog("error: #{error.description}")
-                                                                                                   end
-                                                                                                 end
-                                                 )
-=end
-    @account_store = ACAccountStore.alloc.init
-    @account_type = @account_store.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter);
-    @options = {ACFacebookAppIdKey       => '654511364587200',
-                ACFacebookPermissionsKey => ['email'],
-               }
-    @options = nil
-    @account_store.requestAccessToAccountsWithType(@account_type, options: @options, completion: lambda do |granted, error|
-                                                                                                   if granted
-                                                                                                       accounts = @account_store.accountsWithAccountType(@account_type)
-                                                                                                       anAccount = accounts.lastObject
-                                                                                                       user_id = anAccount.valueForKeyPath("properties.user_id")
-                                                                                                       NSLog("#{user_id}")
-                                                                                                       NSLog("#{anAccount.username}")
-                                                                                                   else
-                                                                                                       NSLog("error: #{error.description}")
-                                                                                                   end
-                                                                                                 end
-                                                 )
+    hoge_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    hoge_button.backgroundColor = UIColor.grayColor
+    hoge_button.sizeToFit
+    hoge_button.frame = CGRectMake(20, 340, self.view.frame.size.width-40, 50)
+    hoge_button.setTitle("Connect with Facebook", forState: UIControlStateNormal)
+    hoge_button.tintColor = UIColor.blackColor
+    self.view.addSubview hoge_button
   end
 
   def push
@@ -65,5 +43,36 @@ class TapController < UIViewController
       super
       self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag: 1)
       self
+  end
+  def connectTwitter
+    @account_store = ACAccountStore.alloc.init
+    @account_type = @account_store.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter);
+    @options = {ACFacebookAppIdKey       => '654511364587200',
+                ACFacebookPermissionsKey => ['email'],
+               }
+    @options = nil
+    user_id = ""
+    user_name = ""
+    completion = lambda do |granted, error|
+        if granted
+            accounts = @account_store.accountsWithAccountType(@account_type)
+            anAccount = accounts.lastObject
+            user_id = anAccount.valueForKeyPath("properties.user_id")
+            NSLog("#{user_id}")
+            user_name = anAccount.username
+            NSLog("#{user_name}")
+            @user = User.new(:twitter_user_id => user_id,
+                             :username => user_name
+                            )
+            alert = UIAlertView.alloc.init
+            alert.message = "user_id: #{@user.twitter_user_id}\nuser_name: #{user_name}"
+            alert.delegate = self
+            alert.addButtonWithTitle "OK"
+            alert.show
+        else
+            NSLog("error: #{error.description}")
+        end
+    end
+    @account_store.requestAccessToAccountsWithType(@account_type, options: @options, completion: completion)
   end
 end
