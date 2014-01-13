c60818bf1b3cea52876095e2650a1df1a7d52c35
nab <napu1031@gmail.com>
2014-01-13 16:14:35 +0900

    add twitter auth

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,19 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    # This is our new line!
+    #@window.rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, alphabet_controller]
+    @window.rootViewController = tab_controller
+
     true
   end
 end
